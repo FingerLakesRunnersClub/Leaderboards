510b499b8dacb6107012f9adaa014010048ee279
Steve Desmond <steve@vtsv.ca>
2020-12-11 14:08:27 -0500

    Add RankedTests

---
+++ ChallengeDashboard.Tests/RankedTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace FLRC.ChallengeDashboard.Tests
+{
+    public class RankedTests
+    {
+        [Fact]
+        public void CanGetTimeBehindToDisplay()
+        {
+            //arrange
+            var rankedResult = new Ranked<Result> { BehindLeader = TimeSpan.Parse("00:01:23.4")};
+
+            //act
+            var behind = rankedResult.BehindLeaderDisplay;
+
+            //assert
+            Assert.Equal("0:01:23.4", behind);
+        }
+    }
+}
