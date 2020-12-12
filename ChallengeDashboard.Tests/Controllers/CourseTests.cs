e152e713ac2f13eba20583a6a4a3260f24fb8d44
Steve Desmond <steve@vtsv.ca>
2020-12-11 19:46:04 -0500

    Add CourseController tests

---
+++ ChallengeDashboard.Tests/Controllers/CourseTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using FLRC.ChallengeDashboard.Controllers;
+using NSubstitute;
+using Xunit;
+
+namespace FLRC.ChallengeDashboard.Tests.Controllers
+{
+    public class CourseTests
+    {
+        [Fact]
+        public async Task ResultsGetCourseInfo()
+        {
+            //arrange
+            var dataService = Substitute.For<IDataService>();
+            var controller = new CourseController(dataService);
+
+            //act
+            var response = await controller.Index(123);
+
+            //assert
+            await dataService.Received().GetCourse(123);
+        }
+    }
+}
