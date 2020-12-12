7cd39b1521781ebbd7d34e73aa8a6bbf4b8ae715
Steve Desmond <steve@vtsv.ca>
2020-12-11 19:34:00 -0500

    Add DashboardTests

---
+++ ChallengeDashboard.Tests/Controllers/DashboardTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FLRC.ChallengeDashboard.Controllers;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using Xunit;
+
+namespace FLRC.ChallengeDashboard.Tests.Controllers
+{
+    public class DashboardTests
+    {
+        [Fact]
+        public async Task CanDashboardGetsAllCourses()
+        {
+            //arrange
+            var config = Substitute.For<IConfiguration>();
+            var dataService = Substitute.For<IDataService>();
+            var controller = new DashboardController(config, dataService);
+
+            //act
+            await controller.Index();
+
+            //assert
+            await dataService.Received().GetAllCourses(Arg.Any<IEnumerable<uint>>());
+        }
+    }
+}
