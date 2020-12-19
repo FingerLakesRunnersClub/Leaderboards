937a03ffc115e496279cdc1ccbaf3379d84782e1
Steve Desmond <steve@vtsv.ca>
2020-12-19 13:17:13 -0500

    Cache teams

--- ChallengeDashboard/Athlete.cs
+++ ChallengeDashboard/Athlete.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FLRC.ChallengeDashboard
 {
@@ -14,6 +16,10 @@ namespace FLRC.ChallengeDashboard
 
         public byte AgeAsOf(DateTime date) => (byte)(date.Subtract(DateOfBirth).TotalDays / 365.2425);
 
-        public Team Team => new Team((byte)(Age / 10));
+        public Team Team => Age < 20 ? Teams[2]
+            : Age >= 80 ? Teams[7]
+            : Teams[(byte)(Age / 10)];
+
+        public static IDictionary<byte, Team> Teams = Enumerable.Range(2, 6).ToDictionary(t => (byte)t, t => new Team((byte)t));
     }
 }
