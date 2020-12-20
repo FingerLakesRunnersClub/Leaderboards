e5370fadc06161381c36fa3bb2f32d98e017dd28
Steve Desmond <steve@vtsv.ca>
2020-12-20 14:31:51 -0500

    Refactor dashboard overall results for performance

--- ChallengeDashboard/DashboardViewModel.cs
+++ ChallengeDashboard/DashboardViewModel.cs
@@ -8,81 +8,77 @@ namespace FLRC.ChallengeDashboard
         public override string Title => "Dashboard";
 
         public override IDictionary<uint, string> CourseNames
-            => CourseResults.Keys.ToDictionary(c => c.ID, c => c.Name);
+            => _courses.ToDictionary(c => c.ID, c => c.Name);
 
         public DashboardViewModel(IEnumerable<Course> courses) => _courses = courses;
 
         public List<DashboardResultsTable> OverallResults
-        {
-            get
-            {
-                var teamResults = _courses.SelectMany(c => c.TeamPoints()).GroupBy(r => r.Team).ToDictionary(g => g.Key, g => g.Sum(r => r.TotalPoints));
-                return new List<DashboardResultsTable>
-                {
-                    new DashboardResultsTable
-                    {
-                        Title = "Top Teams",
-                        ResultType = new FormattedResultType(ResultType.Team),
-                        Rows = teamResults.Select(t => new DashboardResultRow { Name = t.Key.Display, Value = t.Value.ToString() })
-                    }
-                };
-            }
-        }
-
-
-        public IDictionary<Course, List<DashboardResultsTable>> CourseResults => _courses.ToDictionary(c => c, Course =>
-            new List<DashboardResultsTable>
+            => new List<DashboardResultsTable>
             {
                 new DashboardResultsTable
                 {
+                    Title = "Top Teams",
+                    ResultType = new FormattedResultType(ResultType.Team),
+                    Rows = _courses.SelectMany(c => c.TeamPoints())
+                        .GroupBy(r => r.Team)
+                        .ToDictionary(g => g.Key, g => g.Sum(r => r.TotalPoints))
+                        .Select(t => new DashboardResultRow {Name = t.Key.Display, Value = t.Value.ToString()})
+                }
+            };
+
+        public IDictionary<Course, List<DashboardResultsTable>> CourseResults => _courses.ToDictionary(c => c, c =>
+            new List<DashboardResultsTable>
+            {
+                new()
+                {
                     Title = "Fastest (F)",
-                    Course = Course,
+                    Course = c,
                     ResultType = new FormattedResultType(ResultType.Fastest),
                     Category = Category.F,
-                    Rows = Course.Fastest(Category.F).Take(3).Select(r => new DashboardResultRow
+                    Rows = c.Fastest(Category.F).Take(3).Select(r => new DashboardResultRow
                         {Name = r.Athlete.Name, Value = r.Value.Display})
                 },
                 new DashboardResultsTable
                 {
                     Title = "Fastest (M)",
-                    Course = Course,
+                    Course = c,
                     ResultType = new FormattedResultType(ResultType.Fastest),
                     Category = Category.M,
-                    Rows = Course.Fastest(Category.M).Take(3).Select(r => new DashboardResultRow
+                    Rows = c.Fastest(Category.M).Take(3).Select(r => new DashboardResultRow
                         {Name = r.Athlete.Name, Value = r.Value.Display})
                 },
                 new DashboardResultsTable
                 {
                     Title = "Best Average (F)",
-                    Course = Course,
+                    Course = c,
                     ResultType = new FormattedResultType(ResultType.BestAverage),
                     Category = Category.F,
-                    Rows = Course.BestAverage(Category.F).Take(3).Select(r => new DashboardResultRow
+                    Rows = c.BestAverage(Category.F).Take(3).Select(r => new DashboardResultRow
                         {Name = r.Athlete.Name, Value = r.Value.Display})
                 },
                 new DashboardResultsTable
                 {
                     Title = "Best Average (M)",
-                    Course = Course,
+                    Course = c,
                     ResultType = new FormattedResultType(ResultType.BestAverage),
                     Category = Category.M,
-                    Rows = Course.BestAverage(Category.M).Take(3).Select(r => new DashboardResultRow
+                    Rows = c.BestAverage(Category.M).Take(3).Select(r => new DashboardResultRow
                         {Name = r.Athlete.Name, Value = r.Value.Display})
                 },
                 new DashboardResultsTable
                 {
                     Title = "Fastest (Team)",
-                    Course = Course,
+                    Course = c,
                     ResultType = new FormattedResultType(ResultType.Team),
-                    Rows = Course.TeamPoints().OrderByDescending(p => p.AverageAgeGrade).Take(3).Select(r =>
+                    Rows = c.TeamPoints().OrderByDescending(p => p.AverageAgeGrade).Take(3).Select(r =>
                         new DashboardResultRow {Name = r.Team.Display, Value = r.AverageAgeGrade.Display})
                 },
                 new DashboardResultsTable
                 {
                     Title = "Most Runs (Team)",
-                    Course = Course,
+                    Course = c,
                     ResultType = new FormattedResultType(ResultType.Team),
-                    Rows = Course.TeamPoints().OrderByDescending(p => p.TotalRuns).Take(3).Select(r =>
+                    Rows = c.TeamPoints().OrderByDescending(p => p.TotalRuns).Take(3).Select(r =>
                         new DashboardResultRow {Name = r.Team.Display, Value = r.TotalRuns.ToString()})
                 }
             });
