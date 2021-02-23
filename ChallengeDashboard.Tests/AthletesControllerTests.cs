213e40c22baa171b37e13f762fc0a7353d14f23f
Steve Desmond <steve@vtsv.ca>
2021-02-23 14:54:22 -0500

    Add controller/view to display list of all athletes

---
+++ ChallengeDashboard.Tests/AthletesControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FLRC.ChallengeDashboard.Controllers;
+using NSubstitute;
+using Xunit;
+
+namespace FLRC.ChallengeDashboard.Tests
+{
+    public class AthletesControllerTests
+    {
+        [Fact]
+        public async Task CanGetListOfAthletes()
+        {
+            //arrange
+            var athletes = new Dictionary<uint, Athlete>
+            {
+                {123, new Athlete {Name = "Test 1"}},
+                {234, new Athlete {Name = "Test 2"}}
+            };
+            var dataService = Substitute.For<IDataService>();
+            dataService.GetAthletes().Returns(athletes);
+
+            var controller = new AthletesController(dataService);
+            
+            //act
+            var response = await controller.Index();
+            
+            //assert
+            var vm = response.Model as AthletesViewModel;
+            Assert.Equal("Test 1", vm.Athletes[123].Name);
+            Assert.Equal("Test 2", vm.Athletes[234].Name);
+        }
+    }
+}
---
+++ ChallengeDashboard/AthletesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FLRC.ChallengeDashboard
+{
+    public class AthletesViewModel : DataTableViewModel
+    {
+        public override string Title => "All Athletes";
+        public override string ResponsiveBreakpoint => "md";
+        public IDictionary<uint, Athlete> Athletes { get; init; }
+    }
+}
