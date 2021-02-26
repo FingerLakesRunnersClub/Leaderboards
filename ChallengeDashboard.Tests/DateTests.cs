0972f80bfabeae41575e6e7f4ee7f43498fda867
Steve Desmond <steve@vtsv.ca>
2021-02-25 20:29:04 -0500

    Update DateTests for time zone determinism

--- ChallengeDashboard.Tests/DateTests.cs
+++ ChallengeDashboard.Tests/DateTests.cs
@@ -9,13 +9,13 @@ namespace FLRC.ChallengeDashboard.Tests
         public void CanFormatDate()
         {
             //arrange
-            var date = new Date(new DateTime(2000, 1, 1));
+            var date = new Date(new DateTimeOffset(new DateTime(2012, 3, 4, 5, 6, 7, DateTimeKind.Utc)));
             
             //act
             var display = date.Display;
             
             //assert
-            Assert.Equal("1/1/2000 12:00am", display);
+            Assert.Equal("3/4/2012 12:06am", display);
         }
     }
 }
