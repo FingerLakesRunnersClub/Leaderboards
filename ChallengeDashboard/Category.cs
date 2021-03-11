ecd19e1f4fc774875713335131503dd9fa5c03b0
Steve Desmond <steve@vtsv.ca>
2021-03-10 21:31:05 -0500

    Use Category.Equals() for statistics

--- ChallengeDashboard/Category.cs
+++ ChallengeDashboard/Category.cs
@@ -4,8 +4,8 @@ namespace FLRC.ChallengeDashboard
 {
     public class Category : Formatted<FLRC.AgeGradeCalculator.Category?>, IEquatable<Category>
     {
-        public static Category F => new Category(AgeGradeCalculator.Category.F);
-        public static Category M => new Category(AgeGradeCalculator.Category.M);
+        public static Category F => new (AgeGradeCalculator.Category.F);
+        public static Category M => new (AgeGradeCalculator.Category.M);
 
         public Category(FLRC.AgeGradeCalculator.Category? value) : base(value)
         {
