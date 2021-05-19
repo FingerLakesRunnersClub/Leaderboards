179cf66fe5a919986130bcb68f063c9052ad1508
Steve Desmond <steve@vtsv.ca>
2021-05-19 15:11:42 -0400

    Remove explicit timezone declaration

--- ChallengeDashboard/Date.cs
+++ ChallengeDashboard/Date.cs
@@ -7,12 +7,7 @@ namespace FLRC.ChallengeDashboard
         public Date(DateTimeOffset value) : base(value)
         {
         }
-        
-        private static readonly string TimeZoneName = !OperatingSystem.IsWindows()
-            ? "America/New_York"
-            : "Eastern Standard Time";
-        private static readonly TimeZoneInfo TimeZone = TimeZoneInfo.FindSystemTimeZoneById(TimeZoneName);
 
-        public override string Display => TimeZoneInfo.ConvertTime(Value, TimeZone).ToString("M/d/yyyy h:mmtt").ToLower();
-    }
+        public override string Display => Value.ToLocalTime().ToString("M/d/yyyy h:mmtt").ToLower();
+    }   
 }
