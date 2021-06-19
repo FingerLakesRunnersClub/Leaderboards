24eaaeb4ec215aa9f61b69f8aec382a5479c3ce0
Steve Desmond <steve@vtsv.ca>
2021-06-19 14:53:29 -0400

    Add formatted record to display percentages

---
+++ ChallengeDashboard/Percent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FLRC.ChallengeDashboard
+{
+    public record Percent : Formatted<double>, IComparable<Percent>
+    {
+        public Percent(double value) : base(value)
+        {
+        }
+
+        public override string Display => $"{Value:F0}%";
+        public int CompareTo(Percent other) => Value.CompareTo(other.Value);
+    }
+}
