9518c2245d452be533c535b21a8ecbb8842bc3e6
Steve Desmond <steve@vtsv.ca>
2021-06-28 12:52:06 -0400

    Cleanup refactoring mess

--- ChallengeDashboard/Controllers/InvalidController.cs
+++ ChallengeDashboard/Controllers/InvalidController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-namespace FLRC.ChallengeDashboard
-{
-}
 
 namespace FLRC.ChallengeDashboard.Controllers
 {
