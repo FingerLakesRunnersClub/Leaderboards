332410d3f64c41873abd246faec1399ed948a756
Steve Desmond <steve@vtsv.ca>
2021-07-06 14:34:27 -0400

    Add group member list

---
+++ ChallengeDashboard.Tests/GroupControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using FLRC.ChallengeDashboard.Controllers;
+using NSubstitute;
+using Xunit;
+
+namespace FLRC.ChallengeDashboard.Tests
+{
+    public class GroupControllerTests
+    {
+        [Fact]
+        public async Task CanGetGroupMembers()
+        {
+            //arrange
+            var dataService = Substitute.For<IDataService>();
+            dataService.GetGroupMembers("Test").Returns(new[]
+            {
+                CourseData.Athlete1,
+                CourseData.Athlete3
+            });
+            dataService.GetAllResults().Returns(new[] {new Course {Results = CourseData.Results}});
+            var controller = new GroupController(dataService);
+            
+            //act
+            var result = await controller.Index("Test");
+            
+            //assert
+            var vm = (GroupResultsViewModel) result.Model;
+            Assert.Equal(2, vm.RankedResults.Count);
+        }
+    }
+}
---
+++ ChallengeDashboard/GroupResultsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FLRC.ChallengeDashboard
+{
+    public class GroupResultsViewModel : OverallResultsViewModel<TeamMember>
+    {
+        public override string Title => $"{Name} Group Members";
+
+        public string Name { get; init; }
+    }
+}
