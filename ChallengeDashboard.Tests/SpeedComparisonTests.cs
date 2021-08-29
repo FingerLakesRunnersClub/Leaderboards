4b37ac494d9aa9b1f3234e6ab5cee291ee0d7cd4
Steve Desmond <steve@vtsv.ca>
2021-08-29 19:40:30 -0400

    Fix SpeedComparisonTests namespace

--- ChallengeDashboard.Tests/SpeedComparisonTests.cs
+++ ChallengeDashboard.Tests/SpeedComparisonTests.cs
@@ -1,21 +1,23 @@
-using FLRC.ChallengeDashboard;
 using Xunit;
 
-public class SpeedComparisonTests
+namespace FLRC.ChallengeDashboard.Tests
 {
-    [Theory]
-    [InlineData(-5.43, "5.4% faster")]
-    [InlineData(3.56, "3.6% slower")]
-    [InlineData(0, "Same")]
-    public void CanGetSpeedComparisonFromPercentDifference(double value, string expected)
+    public class SpeedComparisonTests
     {
-        //arrange
-        var comparison = new SpeedComparison(value);
+        [Theory]
+        [InlineData(-5.43, "5.4% faster")]
+        [InlineData(3.56, "3.6% slower")]
+        [InlineData(0, "Same")]
+        public void CanGetSpeedComparisonFromPercentDifference(double value, string expected)
+        {
+            //arrange
+            var comparison = new SpeedComparison(value);
 
-        //act
-        var display = comparison.Display;
+            //act
+            var display = comparison.Display;
 
-        //assert
-        Assert.Equal(expected, display);
+            //assert
+            Assert.Equal(expected, display);
+        }
     }
 }
