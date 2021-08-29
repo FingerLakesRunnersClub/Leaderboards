f18b19a4c18e025f0b91f736b3c4dd250e24258b
Steve Desmond <steve@vtsv.ca>
2021-08-29 19:41:06 -0400

    Use referential equality for athlete matching

--- ChallengeDashboard/AthleteSummary.cs
+++ ChallengeDashboard/AthleteSummary.cs
@@ -23,15 +23,15 @@ namespace FLRC.ChallengeDashboard
         public AthleteSummary(Athlete athlete, IReadOnlyCollection<Course> results)
         {
             Athlete = athlete;
-            Fastest = results.ToDictionary(c => c, c => c.Fastest(athlete.Category).FirstOrDefault(r => r.Result.Athlete.ID == athlete.ID));
-            Average = results.ToDictionary(c => c, c => c.BestAverage(athlete.Category).FirstOrDefault(r => r.Result.Athlete.ID == athlete.ID));
-            Runs = results.ToDictionary(c => c, c => c.MostRuns().FirstOrDefault(r => r.Result.Athlete.ID == athlete.ID));
-            All = results.ToDictionary(c => c, c => c.Results.Where(r => r.Athlete.ID == athlete.ID));
+            Fastest = results.ToDictionary(c => c, c => c.Fastest(athlete.Category).FirstOrDefault(r => r.Result.Athlete == athlete));
+            Average = results.ToDictionary(c => c, c => c.BestAverage(athlete.Category).FirstOrDefault(r => r.Result.Athlete == athlete));
+            Runs = results.ToDictionary(c => c, c => c.MostRuns().FirstOrDefault(r => r.Result.Athlete == athlete));
+            All = results.ToDictionary(c => c, c => c.Results.Where(r => r.Athlete == athlete));
             
             var overallViewModel = new OverallResults(results);
-            OverallPoints = overallViewModel.MostPoints(athlete.Category).FirstOrDefault(r => r.Result.Athlete.ID == athlete.ID);
-            OverallAgeGrade = overallViewModel.AgeGrade().FirstOrDefault(r => r.Result.Athlete.ID == athlete.ID);
-            OverallMiles = overallViewModel.MostMiles().FirstOrDefault(r => r.Result.Athlete.ID == athlete.ID);
+            OverallPoints = overallViewModel.MostPoints(athlete.Category).FirstOrDefault(r => r.Result.Athlete == athlete);
+            OverallAgeGrade = overallViewModel.AgeGrade().FirstOrDefault(r => r.Result.Athlete == athlete);
+            OverallMiles = overallViewModel.MostMiles().FirstOrDefault(r => r.Result.Athlete == athlete);
             TeamResults = overallViewModel.TeamPoints().FirstOrDefault(r => r.Team == athlete.Team);
 
             TotalResults = Fastest.Count(r => r.Value != null) + Average.Count(r => r.Value != null);
