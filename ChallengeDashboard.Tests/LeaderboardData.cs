4979ea23cbb51d478f448a94880a187ddcba04c2
Steve Desmond <steve@vtsv.ca>
2021-08-30 12:47:33 -0400

    Add start times to LeaderboardData

--- ChallengeDashboard.Tests/LeaderboardData.cs
+++ ChallengeDashboard.Tests/LeaderboardData.cs
@@ -5,10 +5,10 @@ namespace FLRC.ChallengeDashboard.Tests
 {
     public static class LeaderboardData
     {
-        public static readonly Athlete Athlete1 = new() {ID = 123, Name = "A1", Age = 20, Category = Category.M};
-        public static readonly Athlete Athlete2 = new() {ID = 234, Name = "A2", Age = 30, Category = Category.M};
-        public static readonly Athlete Athlete3 = new() {ID = 345, Name = "A3", Age = 20, Category = Category.F};
-        public static readonly Athlete Athlete4 = new() {ID = 456, Name = "A4", Age = 30, Category = Category.F};
+        public static readonly Athlete Athlete1 = new() {ID = 123, Name = "A1", Age = 20, DateOfBirth = new DateTime(2000, 1, 1), Category = Category.M};
+        public static readonly Athlete Athlete2 = new() {ID = 234, Name = "A2", Age = 30, DateOfBirth = new DateTime(1990, 1, 1), Category = Category.M};
+        public static readonly Athlete Athlete3 = new() {ID = 345, Name = "A3", Age = 20, DateOfBirth = new DateTime(2000, 1, 1), Category = Category.F};
+        public static readonly Athlete Athlete4 = new() {ID = 456, Name = "A4", Age = 30, DateOfBirth = new DateTime(1990, 1, 1), Category = Category.F};
 
         public static readonly IEnumerable<Course> Courses = new List<Course>
         {
@@ -20,16 +20,16 @@ namespace FLRC.ChallengeDashboard.Tests
         private static IEnumerable<Result> Results
             => new List<Result>
             {
-                new() {Course = Course, Athlete = Athlete1, Duration = new Time(new TimeSpan(1, 2, 3))},
-                new() {Course = Course, Athlete = Athlete1, Duration = new Time(new TimeSpan(1, 23, 45))},
-                new() {Course = Course, Athlete = Athlete2, Duration = new Time(new TimeSpan(2, 3, 4))},
-                new() {Course = Course, Athlete = Athlete2, Duration = new Time(new TimeSpan(2, 34, 56))},
-                new() {Course = Course, Athlete = Athlete2, Duration = new Time(new TimeSpan(2, 22, 22))},
-                new() {Course = Course, Athlete = Athlete3, Duration = new Time(new TimeSpan(3, 2, 1))},
-                new() {Course = Course, Athlete = Athlete3, Duration = new Time(new TimeSpan(3, 21, 0))},
-                new() {Course = Course, Athlete = Athlete4, Duration = new Time(new TimeSpan(4, 3, 2))},
-                new() {Course = Course, Athlete = Athlete4, Duration = new Time(new TimeSpan(4, 32, 10))},
-                new() {Course = Course, Athlete = Athlete4, Duration = new Time(new TimeSpan(4, 4, 4))}
+                new() {Course = Course, Athlete = Athlete1, StartTime = new Date(DateTime.Parse("1/2/2020")), Duration = new Time(new TimeSpan(1, 2, 3))},
+                new() {Course = Course, Athlete = Athlete1, StartTime = new Date(DateTime.Parse("1/5/2020")), Duration = new Time(new TimeSpan(1, 23, 45))},
+                new() {Course = Course, Athlete = Athlete2, StartTime = new Date(DateTime.Parse("1/6/2020")), Duration = new Time(new TimeSpan(2, 3, 4))},
+                new() {Course = Course, Athlete = Athlete2, StartTime = new Date(DateTime.Parse("1/3/2020")), Duration = new Time(new TimeSpan(2, 34, 56))},
+                new() {Course = Course, Athlete = Athlete2, StartTime = new Date(DateTime.Parse("1/7/2020")), Duration = new Time(new TimeSpan(2, 22, 22))},
+                new() {Course = Course, Athlete = Athlete3, StartTime = new Date(DateTime.Parse("1/4/2020")), Duration = new Time(new TimeSpan(3, 2, 1))},
+                new() {Course = Course, Athlete = Athlete3, StartTime = new Date(DateTime.Parse("1/9/2020")), Duration = new Time(new TimeSpan(3, 21, 0))},
+                new() {Course = Course, Athlete = Athlete4, StartTime = new Date(DateTime.Parse("1/8/2020")), Duration = new Time(new TimeSpan(4, 3, 2))},
+                new() {Course = Course, Athlete = Athlete4, StartTime = new Date(DateTime.Parse("1/10/2020")), Duration = new Time(new TimeSpan(4, 32, 10))},
+                new() {Course = Course, Athlete = Athlete4, StartTime = new Date(DateTime.Parse("1/1/2020")), Duration = new Time(new TimeSpan(4, 4, 4))}
             };
     }
 }
