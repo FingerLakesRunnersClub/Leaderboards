efb3eb0f9d906a1fb7bb59aece813d7a81abdf8b
Steve Desmond <steve@vtsv.ca>
2021-08-31 17:45:34 -0400

    Rename Completed => Completions in on completed page

--- ChallengeDashboard/CompletedViewModel.cs
+++ ChallengeDashboard/CompletedViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class CompletedViewModel : OverallResultsViewModel<Date>
     {
-        public override string Title => "Completed";
+        public override string Title => "Completions";
         public override string ResponsiveBreakpoint => "lg";
     }
 }
