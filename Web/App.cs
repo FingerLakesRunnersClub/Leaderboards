e4f7f0d723fcb02a21e7aa60f3ef496fc648caa2
Steve Desmond <steve@vtsv.ca>
2022-02-01 16:04:12 -0500

    Add initial trail circuit project

---
+++ Web/App.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace FLRC.Leaderboards.Web;
+
+public static class App
+{
+	public static void Run(string[] args)
+		=> Host.CreateDefaultBuilder(args)
+			.ConfigureWebHostDefaults(app => app.UseStartup<Startup>().UseWebRoot(Path.Combine(AppContext.BaseDirectory, "wwwroot")))
+			.Build()
+			.Run();
+}
