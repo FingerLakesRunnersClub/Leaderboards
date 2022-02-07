59b99ef4734f5af8640cb1f11fc7c6f1aa4adae9
Steve Desmond <steve@vtsv.ca>
2022-02-07 12:32:10 -0500

    Add name to ID extension method

---
+++ Core.Tests/Athletes/NameExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using FLRC.Leaderboards.Core.Athletes;
+using Xunit;
+
+namespace FLRC.Leaderboards.Core.Tests.Athletes;
+
+public class NameExtensionsTests
+{
+	[Fact]
+	public void IdenticalNamesProduceIdenticalIDs()
+	{
+		//arrange
+		const string name1 = "Steve Desmond";
+		const string name2 = "Steve Desmond";
+
+		//act
+		var id1 = name1.GetID();
+		var id2 = name2.GetID();
+
+		//assert
+		Assert.Equal(id1, id2);
+	}
+}
---
+++ Core/Athletes/NameExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FLRC.Leaderboards.Core.Athletes;
+
+public static class NameExtensions
+{
+	public static uint GetID(this string name)
+		=> BitConverter.ToUInt32(MD5.HashData(Encoding.UTF8.GetBytes(name)));
+}
