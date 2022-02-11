281b40a928a329a3877f044852e4081dad8c15b6
Steve Desmond <steve@vtsv.ca>
2022-02-11 10:44:07 -0500

    Allow app title to be configured

--- Core/Config.cs
+++ Core/Config.cs
@@ -5,6 +5,7 @@ namespace FLRC.Leaderboards.Core;
 
 public record Config
 {
+	public string App { get; }
 	public IDictionary<uint, string> CourseNames { get; }
 	public IDictionary<string, string> Links { get; }
 	public FeatureSet Features { get; }
@@ -12,6 +13,7 @@ public record Config
 
 	public Config(IConfiguration config)
 	{
+		App = config.GetValue<string>("App");
 		CourseNames = config.GetSection("Races").GetChildren().ToDictionary(c => c.GetValue<uint>("ID"), c => c.GetValue<string>("Name"));
 		Links = config.GetSection("Links").GetChildren().ToDictionary(c => c.Key, c => c.Value);
 		Features = new FeatureSet(config.GetSection("Features"));
