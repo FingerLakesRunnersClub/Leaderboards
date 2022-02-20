45d59f51be13179524d955229f050fc682afbbdd
Steve Desmond <steve@vtsv.ca>
2022-02-20 13:53:27 -0500

    Only show Team column if age group teams are enabled

--- Core/FeatureSet.cs
+++ Core/FeatureSet.cs
@@ -6,12 +6,14 @@ public record FeatureSet
 {
 	public bool GenerateAthleteID { get; }
 	public bool MultiAttempt { get; }
+	public bool AgeGroupTeams { get; }
 	public bool ShowBadges { get; }
 
 	public FeatureSet(IConfiguration section)
 	{
 		GenerateAthleteID = section.GetValue<bool>(nameof(GenerateAthleteID));
 		MultiAttempt = section.GetValue<bool>(nameof(MultiAttempt));
+		AgeGroupTeams = section.GetValue<bool>(nameof(AgeGroupTeams));
 		ShowBadges = section.GetValue<bool>(nameof(ShowBadges));
 	}
 }
