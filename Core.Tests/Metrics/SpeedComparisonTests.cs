2f1fc950e1008bb42232cc76a60d60b6101b55fc
Steve Desmond <Steve@ecoAPM.com>
2022-04-04 10:10:26 -0400

    Use rounded values for similar athlete's same speed indicator

--- Core.Tests/Metrics/SpeedComparisonTests.cs
+++ Core.Tests/Metrics/SpeedComparisonTests.cs
@@ -9,6 +9,8 @@ public class SpeedComparisonTests
 	[InlineData(-5.43, "5.4% faster")]
 	[InlineData(3.56, "3.6% slower")]
 	[InlineData(0, "Same")]
+	[InlineData(0.09, "Same")]
+	[InlineData(-0.08, "Same")]
 	public void CanGetSpeedComparisonFromPercentDifference(double value, string expected)
 	{
 		//arrange
--- Core/Metrics/SpeedComparison.cs
+++ Core/Metrics/SpeedComparison.cs
@@ -3,5 +3,5 @@ namespace FLRC.Leaderboards.Core.Metrics;
 public record SpeedComparison(double Value) : Formatted<double>(Value)
 {
 	private string Direction => Value < 0 ? "faster" : "slower";
-	public override string Display => Value == 0 ? "Same" : $"{Math.Abs(Value):F1}% {Direction}";
+	public override string Display => Math.Abs(Value) < 0.1 ? "Same" : $"{Math.Abs(Value):F1}% {Direction}";
 }
