c3e4410ef4a06dac0d8bbff6c27affe8ce4e9c6c
Steve Desmond <Steve@ecoAPM.com>
2022-04-12 10:51:29 -0400

    Switch to Font Awesome for community starts icons

--- Web/wwwroot/data-table.js
+++ Web/wwwroot/data-table.js
@@ -1,44 +1,68 @@
-function communityEmoji(points) {
-	return (points.GroupRun ? '🏃' : ' ')
-		+  (points.Story ? '📄' : ' ')
-		+  (points.ShopLocal ? '🏙️' : ' ');
+function communityEmoji(stars) {
+	return `<div class="community d-flex justify-content-center">
+				<span class="group">`
+		+ (stars.GroupRun
+			? `<span class="star" title="Group Run">
+					<i class="fa fa-running">️</i>
+					<i class="fa fa-running">️</i>
+					<i class="fa fa-star"></i>
+				</span>`
+			: '')
+		+ `</span>
+			<span class="group">`
+		+ (stars.Story
+			? `<span class="star" title="Story">
+					<i class="far fa-file-alt"></i>
+					<i class="fa fa-star"></i>
+				</span>`
+			: '')
+		+ `</span>
+			<span class="group">`
+		+ (stars.ShopLocal
+			? `<span class="star" title="Shop Local">
+					<i class="fa fa-shopping-basket"></i>
+					<i class="fa fa-star"></i>
+				</span>`
+			: '')
+		+ `</span>
+		</div>`;
 }
 
 new Vue({
-    data: {
-        fields: fields,
-        sort: {
-            field: null,
-            descending: false
-        },
-        rows: rows,
-        row_style: typeof(row_style) !== 'undefined' ? row_style : null
-    },
-    computed: {
-        show_table: function () {
-            return this.rows.length > 0;
-        },
-        fields_to_show: function () {
-            return this.fields.filter(f => f.show === undefined || f.show === true);
-        },
-        sorted_rows: function () {
-            if (!this.sort.field)
-                this.sort.field = this.fields[0];
-            const sorted = this.rows.concat().sort((r1, r2) => {
-                const v1 = this.sort.field.sort ? this.sort.field.sort(r1) : this.sort.field.value(r1);
-                const v2 = this.sort.field.sort ? this.sort.field.sort(r2) : this.sort.field.value(r2);
-                return v1 > v2 ? 1
-                    : (v1 < v2 ? -1
-                        : 0);
-            });
-            return this.sort.descending ^ this.sort.field.descending ? sorted.reverse() : sorted;
-        }
-    },
-    methods: {
-        toggleSort: function (field, object) {
-            this.sort.descending = (field === this.sort.field) ? !this.sort.descending : false;
-            this.sort.field = field;
-            this.sort.object = object;
-        }
-    }
+	data: {
+		fields: fields,
+		sort: {
+			field: null,
+			descending: false
+		},
+		rows: rows,
+		row_style: typeof (row_style) !== 'undefined' ? row_style : null
+	},
+	computed: {
+		show_table: function () {
+			return this.rows.length > 0;
+		},
+		fields_to_show: function () {
+			return this.fields.filter(f => f.show === undefined || f.show === true);
+		},
+		sorted_rows: function () {
+			if (!this.sort.field)
+				this.sort.field = this.fields[0];
+			const sorted = this.rows.concat().sort((r1, r2) => {
+				const v1 = this.sort.field.sort ? this.sort.field.sort(r1) : this.sort.field.value(r1);
+				const v2 = this.sort.field.sort ? this.sort.field.sort(r2) : this.sort.field.value(r2);
+				return v1 > v2 ? 1
+					: (v1 < v2 ? -1
+						: 0);
+			});
+			return this.sort.descending ^ this.sort.field.descending ? sorted.reverse() : sorted;
+		}
+	},
+	methods: {
+		toggleSort: function (field, object) {
+			this.sort.descending = (field === this.sort.field) ? !this.sort.descending : false;
+			this.sort.field = field;
+			this.sort.object = object;
+		}
+	}
 }).$mount('app');
