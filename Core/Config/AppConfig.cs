763448463c809128edeee57fd6be436a8dc37f56
Steve Desmond <Steve@ecoAPM.com>
2022-04-20 11:40:10 -0400

    Add awards to app config

--- Core/Config/AppConfig.cs
+++ Core/Config/AppConfig.cs
@@ -5,26 +5,32 @@ namespace FLRC.Leaderboards.Core.Config;
 public record AppConfig : IConfig
 {
 	public string App { get; }
+	public IFeatureSet Features { get; }
 	public IDictionary<uint, string> CourseNames { get; }
 	public IDictionary<string, string> Links { get; }
-	public IFeatureSet Features { get; }
 	public IDictionary<string, string> Competitions { get; }
-
+	public IDictionary<string, byte> Awards { get; }
 	public string CommunityURL { get; }
 	public string CommunityKey { get; }
+
 
 	public AppConfig(IConfiguration config)
 	{
 		App = config.GetValue<string>("App");
-		CourseNames = config.GetSection("Races").GetChildren().ToDictionary(c => c.GetValue<uint>("ID"), c => c.GetValue<string>("Name"));
-		Links = GetArray(config.GetSection("Links"));
 		Features = new FeatureSet(config.GetSection("Features"));
-		Competitions = GetArray(config.GetSection("Competitions"));
+		CourseNames = config.GetSection("Races").GetChildren().ToDictionary(c => c.GetValue<uint>("ID"), c => c.GetValue<string>("Name"));
+		Links = GetStringDictionary(config.GetSection("Links"));
+		Competitions = GetStringDictionary(config.GetSection("Competitions"));
+		Awards = GetByteDictionary(config.GetSection("Awards"));
 		CommunityURL = config.GetValue<string>("CommunityURL");
 		CommunityKey = Environment.GetEnvironmentVariable("CommunityKey");
 	}
 
-	private static IDictionary<string, string> GetArray(IConfiguration section)
+	private static IDictionary<string, string> GetStringDictionary(IConfiguration section)
 		=> section.GetChildren().SelectMany(c => c.GetChildren())
 			.ToDictionary(c => c.Key, c => c.Value);
+
+	private static IDictionary<string, byte> GetByteDictionary(IConfiguration section)
+		=> section.GetChildren().SelectMany(c => c.GetChildren())
+			.ToDictionary(c => c.Key, c => byte.Parse(c.Value));
 }
--- Core/Config/IConfig.cs
+++ Core/Config/IConfig.cs
@@ -3,10 +3,11 @@ namespace FLRC.Leaderboards.Core.Config;
 public interface IConfig
 {
 	string App { get; }
+	IFeatureSet Features { get; }
 	IDictionary<uint, string> CourseNames { get; }
 	IDictionary<string, string> Links { get; }
-	IFeatureSet Features { get; }
 	IDictionary<string, string> Competitions { get; }
+	IDictionary<string, byte> Awards { get; }
 	string CommunityURL { get; }
 	string CommunityKey { get; }
 }
