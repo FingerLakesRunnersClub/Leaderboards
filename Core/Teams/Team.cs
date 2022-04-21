0cf36ae597f419dd4664ed4dd7b7d011f5670522
Steve Desmond <Steve@ecoAPM.com>
2022-04-21 11:46:11 -0400

    Add min/max age fields to team

--- Core/Teams/Team.cs
+++ Core/Teams/Team.cs
@@ -2,7 +2,10 @@ namespace FLRC.Leaderboards.Core.Teams;
 
 public record Team(byte Value) : Formatted<byte>(Value)
 {
-	public override string Display => Value == 2 ? "1–29"
-		: Value == 6 ? "60+"
-		: $"{Value}0–{Value}9";
+	public byte MinAge => (byte) (Value >= 3 ? Value * 10 : 1);
+	public byte? MaxAge => Value <= 5 ? (byte) (Value * 10 + 9) : null;
+
+	public override string Display => MaxAge == null
+		? $"{MinAge}+"
+		: $"{MinAge}–{MaxAge}";
 }
