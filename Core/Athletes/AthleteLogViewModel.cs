c5b901ec1a464d4a41846fb202d84b2b59d53d72
Steve Desmond <Steve@ecoAPM.com>
2022-04-22 09:48:04 -0400

    Fix athlete log table overflow on medium viewports

--- Core/Athletes/AthleteLogViewModel.cs
+++ Core/Athletes/AthleteLogViewModel.cs
@@ -8,5 +8,5 @@ public class AthleteLogViewModel : DataTableViewModel
 	public Athlete Athlete { get; init; }
 	public RankedList<Time> Results { get; init; }
 	public override string Title => $"{Athlete.Name} — Activity Log";
-	public override string ResponsiveBreakpoint => "md";
+	public override string ResponsiveBreakpoint => "lg";
 }
