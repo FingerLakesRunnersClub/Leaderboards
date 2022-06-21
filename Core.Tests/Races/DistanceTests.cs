a326bca6bc25fde164d5cbc716799669efa8530c
Steve Desmond <Steve@ecoAPM.com>
2022-06-21 10:34:43 -0400

    Add distance parsing unit tests

--- Core.Tests/Races/DistanceTests.cs
+++ Core.Tests/Races/DistanceTests.cs
@@ -12,6 +12,8 @@ public class DistanceTests
 	[InlineData("1000 m", 1000)]
 	[InlineData("2 km", 2000)]
 	[InlineData("3 K", 3000)]
+	[InlineData("marathon", Distance.MetersPerMarathon)]
+	[InlineData("half marathon", Distance.MetersPerMarathon / 2)]
 	public void CanCalculateMeters(string distance, double expected)
 	{
 		//act
@@ -32,5 +34,24 @@ public class DistanceTests
 
 		//assert
 		Assert.Equal(3, miles);
+	}
+
+	[Fact]
+	public void CanCompareDistances()
+	{
+		//arrange
+		var d1 = new Distance("1km");
+		var d2 = new Distance("2km");
+		var d3 = new Distance("1000m");
+
+		//act
+		var c1 = d1.CompareTo(d2);
+		var c2 = d2.CompareTo(d3);
+		var c3 = d1.CompareTo(d3);
+
+		//assert
+		Assert.Equal(-1, c1);
+		Assert.Equal(1, c2);
+		Assert.Equal(0, c3);
 	}
 }
