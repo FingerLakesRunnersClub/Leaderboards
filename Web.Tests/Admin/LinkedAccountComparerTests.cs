87a41948e3a2ef904cc70104c8736a6d0101d730
Steve Desmond <Steve@ecoAPM.com>
2026-02-13 10:36:59 -0500

    Add method to compare linked accounts

---
+++ Web.Tests/Admin/LinkedAccountComparerTests.cs
@@ -0,0 +1,56 @@
+using FLRC.Leaderboards.Data.Models;
+using FLRC.Leaderboards.Web.Areas.Admin.Services;
+using Xunit;
+
+namespace FLRC.Leaderboards.Web.Tests.Admin;
+
+public sealed class LinkedAccountComparerTests
+{
+	[Fact]
+	public void AccountsMatchWhenTypeAndValueMatch()
+	{
+		//arrange
+		var comparer = new LinkedAccountComparer();
+
+		var a1 = new LinkedAccount { Type = "t1", Value = "v1" };
+		var a2 = new LinkedAccount { Type = "t1", Value = "v1" };
+
+		//act
+		var equal = comparer.Equals(a1, a2);
+
+		//assert
+		Assert.True(equal);
+	}
+
+	[Fact]
+	public void AccountsDoNotMatchWhenValueDoesNotMatch()
+	{
+		//arrange
+		var comparer = new LinkedAccountComparer();
+
+		var a1 = new LinkedAccount { Type = "t1", Value = "v1" };
+		var a2 = new LinkedAccount { Type = "t1", Value = "v2" };
+
+		//act
+		var equal = comparer.Equals(a1, a2);
+
+		//assert
+		Assert.False(equal);
+	}
+
+	[Fact]
+	public void AccountsDoNotMatchWhenTypeDoesNotMatch()
+	{
+		//arrange
+		var comparer = new LinkedAccountComparer();
+
+		var a1 = new LinkedAccount { Type = "t1", Value = "v1" };
+		var a2 = new LinkedAccount { Type = "t2", Value = "v1" };
+
+		//act
+		var equal = comparer.Equals(a1, a2);
+
+		//assert
+		Assert.False(equal);
+	}
+}
---
+++ Web/Areas/Admin/Services/LinkedAccountComparer.cs
@@ -0,0 +1,20 @@
+using FLRC.Leaderboards.Data.Models;
+
+namespace FLRC.Leaderboards.Web.Areas.Admin.Services;
+
+public sealed class LinkedAccountComparer : IEqualityComparer<LinkedAccount>
+{
+	public bool Equals(LinkedAccount x, LinkedAccount y)
+		=> x is not null
+		   && y is not null
+		   && x.Type == y.Type
+		   && x.Value == y.Value;
+
+	public int GetHashCode(LinkedAccount obj)
+	{
+		var hashCode = new HashCode();
+		hashCode.Add(obj.Type, StringComparer.InvariantCultureIgnoreCase);
+		hashCode.Add(obj.Value, StringComparer.InvariantCultureIgnoreCase);
+		return hashCode.ToHashCode();
+	}
+}
