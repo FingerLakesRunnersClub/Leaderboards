da29d8fed09ca79253a8e4cb2f38100ed4d95a41
Steve Desmond <Steve@ecoAPM.com>
2026-03-05 13:50:41 -0500

    Fix whitespace

--- Core.Tests/TestHelpers.cs
+++ Core.Tests/TestHelpers.cs
@@ -8,5 +8,6 @@ public static class TestHelpers
 	public static AppConfig Config => JSONConfig("json/config.json");
 	public static AppConfig TrailConfig => JSONConfig("json/config-trail.json");
 
-	private static AppConfig JSONConfig(string file) => new(new ConfigurationBuilder().AddJsonFile(file).Build());
+	private static AppConfig JSONConfig(string file)
+		=> new(new ConfigurationBuilder().AddJsonFile(file).Build());
 }
