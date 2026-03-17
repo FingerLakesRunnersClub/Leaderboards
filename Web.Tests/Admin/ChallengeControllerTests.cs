9cf4e85b6dec4489f87e92abdbe1db7552e85de9
Steve Desmond <Steve@ecoAPM.com>
2026-03-17 14:37:42 -0400

    Add challenge admin area

---
+++ Web.Tests/Admin/ChallengeControllerTests.cs
@@ -0,0 +1,62 @@
+using FLRC.Leaderboards.Model;
+using FLRC.Leaderboards.Services;
+using FLRC.Leaderboards.Web.Areas.Admin.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Xunit;
+
+namespace FLRC.Leaderboards.Web.Tests.Admin;
+
+public sealed class ChallengeControllerTests
+{
+	[Fact]
+	public async Task CanShowAllChallenges()
+	{
+		//arrange
+		var challengeService = Substitute.For<IChallengeService>();
+		var courseService = Substitute.For<ICourseService>();
+		var controller = new ChallengesController(challengeService, courseService);
+
+		//act
+		var result = await controller.Index();
+
+		//assert
+		Assert.Null(result.ViewName);
+	}
+
+	[Fact]
+	public async Task CanShowEditForm()
+	{
+		//arrange
+		var challengeService = Substitute.For<IChallengeService>();
+		var courseService = Substitute.For<ICourseService>();
+		var controller = new ChallengesController(challengeService, courseService);
+
+		var id = Guid.NewGuid();
+		challengeService.Get(id).Returns(new Challenge());
+
+		//act
+		var result = await controller.Edit(id);
+
+		//assert
+		Assert.Equal("Form", result.ViewName);
+	}
+
+	[Fact]
+	public async Task CanSaveEditedChallenge()
+	{
+		//arrange
+		var challengeService = Substitute.For<IChallengeService>();
+		var courseService = Substitute.For<ICourseService>();
+		var controller = new ChallengesController(challengeService, courseService);
+
+		var id = Guid.NewGuid();
+		challengeService.Get(id).Returns(new Challenge());
+
+		//act
+		var result = await controller.Edit(id, "test2", []);
+
+		//assert
+		Assert.Equal(nameof(ChallengesController.Index), result.ActionName);
+	}
+}
---
+++ Web/Areas/Admin/Controllers/ChallengesController.cs
@@ -0,0 +1,39 @@
+using FLRC.Leaderboards.Model;
+using FLRC.Leaderboards.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FLRC.Leaderboards.Web.Areas.Admin.Controllers;
+
+[Area(nameof(Admin))]
+[Authorize(nameof(Admin))]
+public sealed class ChallengesController(IChallengeService challengeService, ICourseService courseService) : Controller
+{
+	[HttpGet]
+	public async Task<ViewResult> Index()
+	{
+		var challenges = await challengeService.All();
+
+		var vm = new ViewModel<Challenge[]>("Challenges", challenges);
+		return View(vm);
+	}
+
+	[HttpGet]
+	public async Task<ViewResult> Edit(Guid id)
+	{
+		var challenge = await challengeService.Get(id);
+		var vm = new ViewModel<Challenge>("Edit Challenge", challenge);
+		return View("Form", vm);
+	}
+
+	[HttpPost]
+	public async Task<RedirectToActionResult> Edit(Guid id, string name, Guid[] selected)
+	{
+		var challenge = await challengeService.Get(id);
+		var updated = new Challenge { Name = name };
+		var courses = await courseService.GetCourses(selected);
+		await challengeService.Update(challenge, updated);
+		await challengeService.UpdateCourses(challenge, courses);
+		return RedirectToAction(nameof(Index));
+	}
+}
