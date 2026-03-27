7af6aa5dda65bc4fd1e8f48f80e98b93ac495595
Steve Desmond <Steve@ecoAPM.com>
2026-03-27 15:00:54 -0400

    Add ability to get series settings and features by name via dictionary

--- Model/Series.cs
+++ Model/Series.cs
@@ -10,9 +10,12 @@ public record Series : Identifiable<Guid>
 
 	[JsonIgnore]
 	public virtual ICollection<Setting> Settings { get; init; } = [];
+	public IDictionary<string, string> Setting => Settings.ToDictionary(f => f.Key, f => f.Value);
 
 	[JsonIgnore]
 	public virtual ICollection<Feature> Features { get; init; } = [];
+
+	public IDictionary<string, bool> Feature => Features.ToDictionary(f => f.Key, f => f.Value);
 
 	[JsonIgnore]
 	public virtual ICollection<Iteration> Iterations { get; init; } = [];
