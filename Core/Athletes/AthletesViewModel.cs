a87e40950154ceef978625bfc1e45aaa371865f2
Steve Desmond <Steve@ecoAPM.com>
2026-04-03 11:12:00 -0400

    Convert participants list to use new services layer

--- Core/Athletes/AthletesViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace FLRC.Leaderboards.Core.Athletes;
-
-public sealed class AthletesViewModel : DataTableViewModel
-{
-	public override string Title => "Registered Participants";
-	public override string ResponsiveBreakpoint => "md";
-	public IDictionary<uint, Athlete> Athletes { get; init; }
-}
