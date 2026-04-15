674d53c65c112448bfe4ec8b88a637473426bb6b
Steve Desmond <Steve@ecoAPM.com>
2026-04-15 13:58:39 -0400

    Remove erroneously added trail circuit courses from 2026 challenge

---
+++ Data/Migrations/2026-04-15_Fix2026ChallengeCourses.sql
@@ -0,0 +1,8 @@
+DELETE
+FROM RaceIterations
+WHERE iterationid =
+      (SELECT ID FROM Iterations WHERE Name = '2026' and SeriesID = (SELECT ID FROM Series WHERE Key = 'Challenge'))
+  AND RaceID IN (SELECT RaceID
+                 FROM RaceIterations
+                 WHERE IterationID IN
+                       (SELECT ID FROM Iterations WHERE SeriesID IN (SELECT ID FROM Series WHERE Key = 'TrailCircuit')))
