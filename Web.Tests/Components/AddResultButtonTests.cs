c7605b90a21cb54a1636bc6e6d320350ebbbe218
Steve Desmond <Steve@ecoAPM.com>
2026-04-18 18:13:41 -0400

    Add additional conditions to show Add Result button

--- Web.Tests/Components/AddResultButtonTests.cs
+++ Web.Tests/Components/AddResultButtonTests.cs
@@ -1,9 +1,9 @@
+using System.Security.Claims;
 using FLRC.Leaderboards.Core.Config;
 using FLRC.Leaderboards.Model;
 using FLRC.Leaderboards.Services;
 using FLRC.Leaderboards.Web.Components;
 using FLRC.Leaderboards.Web.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using NSubstitute;
 using Xunit;
@@ -13,30 +13,32 @@ namespace FLRC.Leaderboards.Web.Tests.Components;
 public sealed class AddResultButtonTests
 {
 	[Fact]
-	public async Task ButtonDisplaysWhenSelfTimingSetAndLoggedInAndActiveIteration()
+	public async Task ButtonDisplaysWhenAllConditionsMet()
 	{
 		//arrange
+		var iterationManager = Substitute.For<IIterationManager>();
 		var authService = Substitute.For<IAuthService>();
-		var seriesService = Substitute.For<ISeriesService>();
-		var contextProvider = Substitute.For<IContextProvider>();
+		var athleteService = Substitute.For<IAthleteService>();
 
-		var component = new AddResultButton(authService, seriesService, contextProvider);
+		var component = new AddResultButton(iterationManager, authService, athleteService);
+
+		var iteration = new Iteration
+		{
+			StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+			EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)),
+			Series = new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] }
+		};
+		iterationManager.ActiveIteration().Returns(iteration);
 
 		authService.IsLoggedIn().Returns(true);
-		seriesService.Find(Arg.Any<string>()).Returns(new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] });
+		authService.GetCurrentUser().Returns(new ClaimsPrincipal(new ClaimsIdentity([new Claim("external_id", "123")])));
+
+		var athlete = new Athlete { Registrations = [iteration] };
+		athleteService.Find(Arg.Any<string>(), Arg.Any<string>()).Returns(athlete);
+
+		var course = new Course { ID = Guid.NewGuid(), Race = new Race { Iterations = [iteration] } };
 
 		//act
-		var course = new Course
-		{
-			ID = Guid.NewGuid(),
-			Race = new Race
-			{
-				Iterations =
-				[
-					new Iteration { StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)), EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)) }
-				]
-			}
-		};
 		var result = await component.InvokeAsync(course);
 
 		//assert
@@ -47,27 +49,29 @@ public sealed class AddResultButtonTests
 	public async Task ButtonNotShownWhenNotSelfTiming()
 	{
 		//arrange
+		var iterationManager = Substitute.For<IIterationManager>();
 		var authService = Substitute.For<IAuthService>();
-		var seriesService = Substitute.For<ISeriesService>();
-		var contextProvider = Substitute.For<IContextProvider>();
+		var athleteService = Substitute.For<IAthleteService>();
 
-		var component = new AddResultButton(authService, seriesService, contextProvider);
+		var component = new AddResultButton(iterationManager, authService, athleteService);
+
+		var iteration = new Iteration
+		{
+			StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+			EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)),
+			Series = new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = false }] }
+		};
+		iterationManager.ActiveIteration().Returns(iteration);
 
 		authService.IsLoggedIn().Returns(true);
-		seriesService.Find(Arg.Any<string>()).Returns(new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = false }] });
+		authService.GetCurrentUser().Returns(new ClaimsPrincipal(new ClaimsIdentity([new Claim("external_id", "123")])));
+
+		var athlete = new Athlete { Registrations = [iteration] };
+		athleteService.Find(Arg.Any<string>(), Arg.Any<string>()).Returns(athlete);
+
+		var course = new Course { ID = Guid.NewGuid(), Race = new Race { Iterations = [iteration] } };
 
 		//act
-		var course = new Course
-		{
-			ID = Guid.NewGuid(),
-			Race = new Race
-			{
-				Iterations =
-				[
-					new Iteration { StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)), EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)) }
-				]
-			}
-		};
 		var result = await component.InvokeAsync(course);
 
 		//assert
@@ -78,27 +82,28 @@ public sealed class AddResultButtonTests
 	public async Task ButtonNotShownWhenNotLoggedIn()
 	{
 		//arrange
+		var iterationManager = Substitute.For<IIterationManager>();
 		var authService = Substitute.For<IAuthService>();
-		var seriesService = Substitute.For<ISeriesService>();
-		var contextProvider = Substitute.For<IContextProvider>();
+		var athleteService = Substitute.For<IAthleteService>();
 
-		var component = new AddResultButton(authService, seriesService, contextProvider);
+		var component = new AddResultButton(iterationManager, authService, athleteService);
+
+		var iteration = new Iteration
+		{
+			StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+			EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)),
+			Series = new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] }
+		};
+		iterationManager.ActiveIteration().Returns(iteration);
 
 		authService.IsLoggedIn().Returns(false);
-		seriesService.Find(Arg.Any<string>()).Returns(new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] });
+
+		var athlete = new Athlete { Registrations = [iteration] };
+		athleteService.Find(Arg.Any<string>(), Arg.Any<string>()).Returns(athlete);
+
+		var course = new Course { ID = Guid.NewGuid(), Race = new Race { Iterations = [iteration] } };
 
 		//act
-		var course = new Course
-		{
-			ID = Guid.NewGuid(),
-			Race = new Race
-			{
-				Iterations =
-				[
-					new Iteration { StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)), EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)) }
-				]
-			}
-		};
 		var result = await component.InvokeAsync(course);
 
 		//assert
@@ -109,27 +114,62 @@ public sealed class AddResultButtonTests
 	public async Task ButtonNotShownWhenNoActiveIteration()
 	{
 		//arrange
+		var iterationManager = Substitute.For<IIterationManager>();
 		var authService = Substitute.For<IAuthService>();
-		var seriesService = Substitute.For<ISeriesService>();
-		var contextProvider = Substitute.For<IContextProvider>();
+		var athleteService = Substitute.For<IAthleteService>();
 
-		var component = new AddResultButton(authService, seriesService, contextProvider);
+		var component = new AddResultButton(iterationManager, authService, athleteService);
+
+		var iteration = new Iteration
+		{
+			StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-2)),
+			EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+			Series = new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] }
+		};
+		iterationManager.ActiveIteration().Returns(iteration);
 
 		authService.IsLoggedIn().Returns(true);
-		seriesService.Find(Arg.Any<string>()).Returns(new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] });
+		authService.GetCurrentUser().Returns(new ClaimsPrincipal(new ClaimsIdentity([new Claim("external_id", "123")])));
+
+		var athlete = new Athlete { Registrations = [iteration] };
+		athleteService.Find(Arg.Any<string>(), Arg.Any<string>()).Returns(athlete);
+
+		var course = new Course { ID = Guid.NewGuid(), Race = new Race { Iterations = [iteration] } };
 
 		//act
-		var course = new Course
+		var result = await component.InvokeAsync(course);
+
+		//assert
+		Assert.IsType<ContentViewComponentResult>(result);
+	}
+
+	[Fact]
+	public async Task ButtonNotShownWhenCourseNotInActiveIteration()
+	{
+		//arrange
+		var iterationManager = Substitute.For<IIterationManager>();
+		var authService = Substitute.For<IAuthService>();
+		var athleteService = Substitute.For<IAthleteService>();
+
+		var component = new AddResultButton(iterationManager, authService, athleteService);
+
+		var iteration = new Iteration
 		{
-			ID = Guid.NewGuid(),
-			Race = new Race
-			{
-				Iterations =
-				[
-					new Iteration { StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-2)), EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)) }
-				]
-			}
+			StartDate = DateOnly.FromDateTime(DateTime.Today.AddDays(-1)),
+			EndDate = DateOnly.FromDateTime(DateTime.Today.AddDays(1)),
+			Series = new Series { Features = [new Feature { SeriesID = Guid.NewGuid(), Key = nameof(FeatureSet.SelfTiming), Value = true }] }
 		};
+		iterationManager.ActiveIteration().Returns(iteration);
+
+		authService.IsLoggedIn().Returns(true);
+		authService.GetCurrentUser().Returns(new ClaimsPrincipal(new ClaimsIdentity([new Claim("external_id", "123")])));
+
+		var athlete = new Athlete { Registrations = [iteration] };
+		athleteService.Find(Arg.Any<string>(), Arg.Any<string>()).Returns(athlete);
+
+		var course = new Course { ID = Guid.NewGuid(), Race = new Race { Iterations = [] } };
+
+		//act
 		var result = await component.InvokeAsync(course);
 
 		//assert
--- Web/Components/AddResultButton.cs
+++ Web/Components/AddResultButton.cs
@@ -3,21 +3,46 @@ using FLRC.Leaderboards.Model;
 using FLRC.Leaderboards.Services;
 using FLRC.Leaderboards.Web.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 namespace FLRC.Leaderboards.Web.Components;
 
-public class AddResultButton(IAuthService authService, ISeriesService seriesService, IContextProvider contextProvider) : ViewComponent
+public class AddResultButton(IIterationManager iterationManager, IAuthService authService, IAthleteService athleteService) : ViewComponent
 {
-	private static DateOnly Today => DateOnly.FromDateTime(DateTime.Now);
-
 	public async Task<IViewComponentResult> InvokeAsync(Course course)
 	{
 		var loggedIn = authService.IsLoggedIn();
-		var series = await seriesService.Find(contextProvider.App);
-		var hasActiveIteration = course.Race.Iterations.Any(i => Today >= i.StartDate && Today <= i.EndDate);
+		if (!loggedIn)
+			return Empty;
 
-		return loggedIn && series.Feature[nameof(FeatureSet.SelfTiming)] && hasActiveIteration
-			? View("../../../Results/AddResultButton", course.ID)
-			: Content(string.Empty);
+		var iteration = await iterationManager.ActiveIteration();
+		var iterationIsActive = iteration?.IsActive ?? false;
+		if (!iterationIsActive)
+			return Empty;
+
+		var hasSelfTiming = iteration.Series.Feature[nameof(FeatureSet.SelfTiming)];
+		if (!hasSelfTiming)
+			return Empty;
+
+		var courseIsInActiveIteration = course.Race.Iterations.Contains(iteration);
+		if (!courseIsInActiveIteration)
+			return Empty;
+
+		var athlete = await CurrentAthlete();
+		var athleteIsRegistered = athlete?.Registrations.Contains(iteration) ?? false;
+		if (!athleteIsRegistered)
+			return Empty;
+
+		return View("../../../Results/AddResultButton", course.ID);
+	}
+
+	private ContentViewComponentResult Empty => Content(string.Empty);
+
+	private async Task<Athlete> CurrentAthlete()
+	{
+		var user = authService.GetCurrentUser();
+		var claims = user.ClaimDictionary;
+
+		return await athleteService.Find(LinkedAccount.Keys.Discourse, claims["external_id"]);
 	}
 }
