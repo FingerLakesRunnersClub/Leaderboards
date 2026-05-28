6de2fd10dca8967a893b873266930356742c3576
Steve Desmond <Steve@ecoAPM.com>
2026-05-28 11:35:25 -0400

    Remove unused legacy filter and results extensions

--- Core/Results/ResultsExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace FLRC.Leaderboards.Core.Results;
-
-public static class ResultsExtensions
-{
-	public static Result[] Filter(this Result[] results, Filter filter)
-		=> results.Where(r => filter == null || filter.IsMatch(r)).ToArray();
-}
