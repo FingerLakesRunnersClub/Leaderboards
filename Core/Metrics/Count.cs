7a8d768498f3a3acae6dab9458da056f70702fa0
Steve Desmond <Steve@ecoAPM.com>
2026-05-29 12:05:32 -0400

    Seal count metric

--- Core/Metrics/Count.cs
+++ Core/Metrics/Count.cs
@@ -1,6 +1,6 @@
 namespace FLRC.Leaderboards.Core.Metrics;
 
-public record Count(ushort Value) : Formatted<ushort>(Value), IComparable<Count>
+public sealed record Count(ushort Value) : Formatted<ushort>(Value), IComparable<Count>
 {
 	public override string Display
 		=> Value.ToString("N0");
