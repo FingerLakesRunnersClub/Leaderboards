2ded8ec7346e404ce68dbee5a2f0a87aa8c5f667
Steve Desmond <Steve@ecoAPM.com>
2026-05-29 12:06:15 -0400

    Strongly-type base layout view model

--- Core/ViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace FLRC.Leaderboards.Core;
-
-public abstract record ViewModel
-{
-	public abstract string Title { get; }
-}
--- Web/ViewModels/ViewModel.cs
+++ Web/ViewModels/ViewModel.cs
@@ -1,6 +1,9 @@
 namespace FLRC.Leaderboards.Web.ViewModels;
 
-public sealed record ViewModel<T>(string Title, T Data)
+public sealed record ViewModel<T>(string Title, T Data) : ViewModel;
+
+public abstract record ViewModel
 {
+	public abstract string Title { get; init; }
 	public string Error { get; init; }
 }
