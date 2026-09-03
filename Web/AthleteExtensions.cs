4e2839721687269912f1408d6bcee45c275b9cd4
Steve Desmond <Steve@ecoAPM.com>
2026-09-03 10:31:33 -0400

    Cache an athlete's team for each iteration

--- Web/AthleteExtensions.cs
+++ Web/AthleteExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using FLRC.Leaderboards.Core.Teams;
 using FLRC.Leaderboards.Model;
 
@@ -5,16 +6,21 @@ namespace FLRC.Leaderboards.Web;
 
 public static class AthleteExtensions
 {
+	private static readonly ConcurrentDictionary<(Athlete, Iteration), Team> TeamCache = new();
+
 	extension(Athlete athlete)
 	{
 		public Team Team(Iteration iteration)
 		{
+			if (TeamCache.TryGetValue((athlete, iteration), out var cached))
+				return cached;
+
 			if (iteration?.StartDate is null)
 				return null;
 
 			var ageOnStart = athlete.AgeAsOf(iteration.StartDate.Value);
 			var team = Core.Teams.Team.Teams.First(t => ageOnStart >= t.Value.MinAge && ageOnStart <= (t.Value.MaxAge ?? byte.MaxValue));
-			return team.Value;
+			return TeamCache.AddOrUpdate((athlete, iteration), team.Value, (_, _) => team.Value);
 		}
 	}
 }
