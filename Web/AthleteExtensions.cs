c7c8cb9f0e75f8fc06ab8fdc97d1c60cadcfb99d
Steve Desmond <Steve@ecoAPM.com>
2026-09-06 14:09:08 -0400

    Fix memory leak in athlete team cache lookup

--- Web/AthleteExtensions.cs
+++ Web/AthleteExtensions.cs
@@ -6,21 +6,21 @@ namespace FLRC.Leaderboards.Web;
 
 public static class AthleteExtensions
 {
-	private static readonly ConcurrentDictionary<(Athlete, Iteration), Team> TeamCache = new();
+	private static readonly ConcurrentDictionary<(Guid, Guid), Team> TeamCache = new();
 
 	extension(Athlete athlete)
 	{
 		public Team Team(Iteration iteration)
 		{
-			if (TeamCache.TryGetValue((athlete, iteration), out var cached))
+			if (TeamCache.TryGetValue((athlete.ID, iteration.ID), out var cached))
 				return cached;
 
-			if (iteration?.StartDate is null)
+			if (iteration.StartDate is null)
 				return null;
 
 			var ageOnStart = athlete.AgeAsOf(iteration.StartDate.Value);
 			var team = Core.Teams.Team.Teams.First(t => ageOnStart >= t.Value.MinAge && ageOnStart <= (t.Value.MaxAge ?? byte.MaxValue));
-			return TeamCache.AddOrUpdate((athlete, iteration), team.Value, (_, _) => team.Value);
+			return TeamCache.AddOrUpdate((athlete.ID, iteration.ID), team.Value, (_, _) => team.Value);
 		}
 	}
 }
