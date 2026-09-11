ea6563e0600b8ec957106a2c8b339c89fce0e7c6
Steve Desmond <Steve@ecoAPM.com>
2026-09-11 08:38:32 -0400

    Move configuring importer services out of the top-level program

--- Importer/App.cs
+++ Importer/App.cs
@@ -1,11 +1,22 @@
 using System.IO.Abstractions;
 using System.Text.Json;
+using FLRC.Leaderboards.Core.Config;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace FLRC.Leaderboards.Importer;
 
 public sealed class App(IFileSystem fs, Importer importer, Action<string> log) : IHostedService
 {
+	public static void Services(HostBuilderContext context, IServiceCollection services)
+	{
+		Web.App.ConfigureServices(services);
+		services.AddSingleton<IContextProvider>(_ => new AppContextProvider("Challenge"));
+		services.AddSingleton<Action<string>>(Console.WriteLine);
+		services.AddSingleton<Importer>();
+		services.AddHostedService<App>();
+	}
+
 	public async Task StartAsync(CancellationToken cancellationToken)
 	{
 		await Run("Challenge");
--- Importer/Program.cs
+++ Importer/Program.cs
@@ -1,18 +1,8 @@
-﻿using FLRC.Leaderboards.Core.Config;
-using FLRC.Leaderboards.Importer;
-using Microsoft.Extensions.DependencyInjection;
+﻿using FLRC.Leaderboards.Importer;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateDefaultBuilder(args);
-builder.ConfigureServices((_, services) =>
-	{
-		FLRC.Leaderboards.Web.App.ConfigureServices(services);
-		services.AddSingleton<IContextProvider>(_ => new AppContextProvider("Challenge"));
-		services.AddSingleton<Action<string>>(Console.WriteLine);
-		services.AddSingleton<Importer>();
-		services.AddHostedService<App>();
-	}
-);
+builder.ConfigureServices(App.Services);
 
 var app = builder.Build();
 FLRC.Leaderboards.Web.App.Initialize(app.Services);
